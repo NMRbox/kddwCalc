6042bf2623a77d5da1799a43becc4784c68caa1a
Alex Rizzo <alex.rizzo2000@gmail.com>
2017-07-01 09:08:14 -0400

    Added absFactory class and two subclass for methyls and amides

--- src/edu/uconn/kdCalcdata/AbsFactory.java
+++ src/edu/uconn/kdCalcdata/AbsFactory.java
@@ -1,8 +1,21 @@
 // created by Alex Rizzo on 170701
 
+// recieved from UI
+// ArrayList of paths to object
+// ArrayList of ligand concentrations
+// ArrayList of receptor concentrations
+// multiplier - the value for scaling the two nuclei
+
+// important note:
+// for the three ArrayList objects, each index matches the same
+// indicies for the other two. for instance, index[0] has the 
+// receptor and ligand concentrations and the chemical shifts for
+// the first titration point. these are the data from teh first line of the
+// GUI that requests the data
+
 package edu.uconn.kdCalcdata;
 
 public class AbsFactory 
 {
-
+    private final ArrayList<>
 }
