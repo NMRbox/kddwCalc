6748e5ca8e9c1b2d157f1c419e929bff3f0c47ea
Alex Rizzo <alex.rizzo2000@gmail.com>
2017-07-09 17:29:10 -0400

    edited the GUI stuff. The two comboboxs have variables and seems to work together.

--- src/edu/uconn/kdCalc/gui/InputGUIController.java
+++ src/edu/uconn/kdCalc/gui/InputGUIController.java
@@ -7,23 +7,46 @@ package edu.uconn.kdCalc.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 /**
  *
  * @author home
  */
-public class InputGUIController implements Initializable {
+public class InputGUIController implements Initializable 
+{
+    @FXML
+    public ComboBox<String> typeOfTitration; 
     
- 
+    @FXML
+    public ComboBox<String> resonanceOrderSelector;
+        
+    
+    ObservableList<String> listOfSpectra = 
+        FXCollections.observableArrayList("1H-15N HSQC", "1H-13C methyl HMQC");
     
     @Override
     public void initialize(URL url, ResourceBundle rb) 
     {
-        // TODO
+        typeOfTitration.setItems(listOfSpectra);
+        resonanceOrderSelector.setItems(FXCollections.observableArrayList("Please select type of spectrum first"));
+        
+        
     }    
     
+    
+    public void typeOfTitrationSelected(ActionEvent event)
+    {
+        if(typeOfTitration.getValue().equals("1H-15N HSQC"))
+            resonanceOrderSelector.setItems(FXCollections.observableArrayList("Nitrogen Proton", "Proton Nitrogen"));
+        
+        if(typeOfTitration.getValue().equals("1H-13C methyl HMQC"))
+            resonanceOrderSelector.setItems(FXCollections.observableArrayList("Carbon Proton", "Proton Carbon"));
+    }
 }
