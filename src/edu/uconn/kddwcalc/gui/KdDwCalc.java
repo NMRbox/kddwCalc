b1dfe3e486ac94e1e6b7e9f24aceac63102bba23
Alex Rizzo <alex.rizzo2000@gmail.com>
2017-08-27 12:24:18 -0400

    adding javadoc to gui, not finished

--- src/edu/uconn/kddwcalc/gui/KdDwCalc.java
+++ src/edu/uconn/kddwcalc/gui/KdDwCalc.java
@@ -1,20 +1,28 @@
-// created by AR. edited on 170807 when linking GUI to data input (data inputing was just tested successfully)
-
 package edu.uconn.kddwcalc.gui;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-public class KdDwCalc extends Application 
-{
+/**
+ * Main application class that launches program
+ * 
+ * @author Alex R
+ */
+public class KdDwCalc extends Application {
     
+    /**
+     * Gets FXML and loads scene and stage
+     * 
+     * @param stage the primary stage to show
+     * 
+     * @throws IOException if problem occurs while loading .fxml file 
+     */
     @Override
-    public void start(Stage stage) throws Exception 
-    {
+    public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("SlowExchangeGUI.fxml"));
         
         Scene scene = new Scene(root);
@@ -24,13 +32,11 @@ public class KdDwCalc extends Application
     }
 
     /**
+     * Program <code>main</code> methods where execution starts.
+     * 
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         launch(args);
-        
-        
     }
-    
-}
+} // end class KdDwCalc
