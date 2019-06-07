a51db73271440fccd42f1ac6815c589249b72812
gweatherby <gerardw@alum.mit.edu>
2019-06-07 16:08:09 -0400

    Convert to Maven layout, add Java FX 11 wrapper

---
+++ src/main/java/edu/uconn/kddwcalc/input/AmideNitrogenProtonFactory.java
@@ -0,0 +1,76 @@
+package edu.uconn.kddwcalc.input;
+
+import edu.uconn.kddwcalc.data.AmideNitrogen;
+import edu.uconn.kddwcalc.data.AmideNitrogenProtonTitrationPoint;
+import edu.uconn.kddwcalc.data.AmideProton;
+import edu.uconn.kddwcalc.data.Resonance;
+import edu.uconn.kddwcalc.data.TitrationPoint;
+import edu.uconn.kddwcalc.input.AbsFactory;
+import java.util.Scanner;
+
+/**
+ * A concrete factory class used to create the read in 1H-15N HSQC NMR titration data. Note that the first resonance 
+ * is a {@link AmideNitrogen} and the second resonance is a {@link AmideProton}
+ * 
+ * @author Alex R.
+ * 
+ * @see Resonance
+ * @see TitrationPoint
+ * @see AmideNitrogenProtonTitrationPoint
+ * @see edu.uconn.kddwcalc.data.Titration
+ * @see edu.uconn.kddwcalc.data.TitrationSeries
+ * @see AmideProton
+ * @see AmideNitrogen
+ * 
+ * @since 1.8
+ */
+public class AmideNitrogenProtonFactory extends AbsFactory {
+    
+    /**
+     * A method to read a {@link AmideNitrogen} chemical shift from a peak list
+     * 
+     * @param scanner the object to read in the data from the peak list file
+     * 
+     * @return a {@link AmideNitrogen} subclass of {@link Resonance}
+     */
+    @Override
+    public Resonance getFirstResonanceSubclass(Scanner scanner) {
+        return AmideNitrogen.validateAndCreate(scanner.nextDouble());
+    }
+    
+    /**
+     * A method to read a {@link AmideProton} chemical shift from a peak list
+     * 
+     * @param scanner the object to read in the data from the peak list file
+     * 
+     * @return a {@link AmideProton} subclass of {@link Resonance}
+     */
+    @Override
+    public Resonance getSecondResonanceSubclass(Scanner scanner) {
+        return AmideProton.validateAndCreate(scanner.nextDouble());
+    }
+    
+    /**
+     * A method to create a concrete {@link AmideNitrogenProtonTitrationPoint} subclass of
+     * {@link TitrationPoint}
+     * 
+     * @param ligandConc the ligand concentration
+     * @param receptorConc the receptor concentration
+     * @param firstCoordinate the first {@link Resonance}, here is a {@link AmideNitrogen}
+     * @param secondCoordinate the first {@link Resonance}, here is a {@link AmideProton}
+     * 
+     * @return a fully initialized {@link AmideNitrogenProtonTitrationPoint} concrete subclass of
+     * {@link Resonance}
+     */
+    @Override
+    public TitrationPoint makeTitrationPointSubclass(double ligandConc, 
+                                                     double receptorConc, 
+                                                     Resonance firstCoordinate, 
+                                                     Resonance secondCoordinate) {
+        
+        return AmideNitrogenProtonTitrationPoint.validateAndCreate(ligandConc, 
+                                                                   receptorConc, 
+                                                                   firstCoordinate, 
+                                                                   secondCoordinate);
+    }
+}
---
+++ src/main/java/edu/uconn/kddwcalc/input/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides the classes necessary to take user input and turn it into an
+ * object that can be calculated
+ * 
+ */
+package edu.uconn.kddwcalc.input;
